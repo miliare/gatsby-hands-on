5863030241f00ee90d7c34b2734b5e10e07f9955
Rémy Milia <milia.remy@gmail.com>
2019-01-16 22:14:11 +0100

    add pages

---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default () => (
+  <div>
+    <h2>It's all about the Crafts Academy</h2>
+  </div>
+)
---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default () => (
+  <div>
+    <h2> Email us at the address below:</h2>
+    <p>
+      <a href="mailto:crafts-academy@me.com">crafts-academy@me.com</a>
+    </p>
+  </div>
+)
