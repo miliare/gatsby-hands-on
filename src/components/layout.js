ca328d1f008bfea3708508428fa56686c18d7f6e
Rémy Milia <milia.remy@gmail.com>
2019-01-16 22:28:55 +0100

    components extends react component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,13 @@
-import React from "react"
+import React from "react";
 
-export default ({ children }) => (
-  <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
-    {children}
-  </div>
-)
+class Layout extends React.Component {
+  render() {
+    return (
+      <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default Layout;
